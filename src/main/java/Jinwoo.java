a985df76123c3b501e76ccb9132b65f2d1a7f82c
yuntaeseo <aidanseo5@gmail.com>
2025-08-24 21:33:07 +0800

    Rename into Jinwoo initial skeleton version

---
+++ src/main/java/Jinwoo.java
@@ -0,0 +1,18 @@
+public class Jinwoo {
+    public static void main(String[] args) {
+        String logo = """
+                      _ ___ _  ___      _____   ___ \s
+                  _ | |_ _| \\| \\ \\    / / _ \\ / _ \\\s
+                 | || || || .` |\\ \\/\\/ / (_) | (_) |
+                  \\__/|___|_|\\_| \\_/\\_/ \\___/ \\___/ \
+                """;
+        System.out.println(logo);
+        System.out.println("________________________________________");
+        System.out.println("Hello! I'm Jinwoo\nWhat can I do for you?\n");
+        System.out.println("________________________________________");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("________________________________________");
+
+    }
+}
+
