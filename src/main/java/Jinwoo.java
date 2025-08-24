4dac9d70453d8354c4f152b04327069bbb93443f
yuntaeseo <aidanseo5@gmail.com>
2025-08-24 22:35:07 +0800

    Add echoing user voice by the Shadow Lord

--- src/main/java/Jinwoo.java
+++ src/main/java/Jinwoo.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
+
 public class Jinwoo {
+
+    public static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                return;
+            } else {
+                System.out.println("________________________________________");
+                System.out.println(input);
+                System.out.println("\n________________________________________");
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         String logo = """
                       _ ___ _  ___      _____   ___ \s
@@ -8,10 +26,14 @@ public class Jinwoo {
                 """;
         System.out.println(logo);
         System.out.println("________________________________________");
-        System.out.println("Hello! I'm Jinwoo\nWhat can I do for you?\n");
+        System.out.println("Hello! I'm Jinwoo, the Shadow Monarch\nWhat can I do for you?");
         System.out.println("________________________________________");
-        System.out.println("Bye. Hope to see you again soon!\n");
+        echo();
         System.out.println("________________________________________");
+        System.out.println("Bye Hunter ???. Hope to see you again soon.");
+        System.out.println("________________________________________");
+
+
 
     }
 }
