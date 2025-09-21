2e14cd876be5065c0acf98baf718994e38cf0468
yuntaeseo <aidanseo5@gmail.com>
2025-09-21 18:41:12 +0800

    Add Gui title and docs/Ui.png

--- src/main/java/jinwoo/gui/Main.java
+++ src/main/java/jinwoo/gui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Jinwoo Chatbot");
             fxmlLoader.<MainWindow>getController().setJinwoo(jinwoo);
             stage.show();
         } catch (IOException e) {
